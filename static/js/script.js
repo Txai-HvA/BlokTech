f5fbe98e0e068b687eb321da66edad9aaa1ca90f
Txai-HvA <txai.pereira.de.lacerda@hva.nl>
2021-05-27 20:49:34 +0200

    Removed dead code

--- static/js/script.js
+++ static/js/script.js
@@ -2,26 +2,11 @@ let filterButton = document.querySelector("#filterButton");
 let filterMenu = document.querySelector("#filterMenu");
 let filterDoneButton = document.querySelector("#filterDoneButton");
 
-// let filterGenreInput = document.querySelector("#genreSearch");
-// let filterGenreList = document.querySelector("#filterMenu ul:nth-of-type(1)");
-// let addGenreButton = document.querySelector("#addGenreButton");
-
+//Shows/Hides Filter menu
 function toggleFilterMenu() {
     filterMenu.classList.toggle("showFilterMenu");
     filterMenu.scrollIntoView({ behavior: "smooth" });
 }
-
-// function addGenreToFilters(event) {
-//     event.preventDefault();
-//     let newGenre = filterGenreInput.value;
-//     console.log("0-- " + newGenre);
-
-//     let li = document.createElement("li");
-//     li.appendChild(document.createTextNode(newGenre));
-//     filterGenreList.appendChild(li);
-// }
-
-// addGenreButton.addEventListener("click", addGenreToFilters);
 
 filterButton.addEventListener("click", toggleFilterMenu);
 filterDoneButton.addEventListener("click", toggleFilterMenu);
